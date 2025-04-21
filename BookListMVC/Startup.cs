9385c005c91f2bcfcc60c4dda24659647bcfc857
fatou <fgtpcrypto@gmail.com>
2025-04-21 18:00:27 -0400

    Deploiement

--- BookListMVC/Startup.cs
+++ BookListMVC/Startup.cs
@@ -29,6 +29,7 @@ namespace BookListMVC
             services.AddDbContext<ApplicationDbContext>(
             options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
             );
+            services.AddAuthorization();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
