b8aaa39310e1d9584f0512fb51023a5f3fce35f0
fatou <fgtpcrypto@gmail.com>
2025-04-29 01:21:16 -0400

    Selenium Test

--- BookListSeleniumTests/UnitTest1.cs
+++ BookListSeleniumTests/UnitTest1.cs
@@ -12,7 +12,6 @@ namespace BookListSeleniumTests
         public UnitTest1()
         {
             var options = new ChromeOptions();
-            //options.AddArgument("--headless"); 
             options.AddArgument("--start-maximized");
             options.AddArgument("--no-sandbox");
             options.AddArgument("--disable-dev-shm-usage");
@@ -23,7 +22,7 @@ namespace BookListSeleniumTests
         }
 
         [Fact]
-        public void Accueil_AfficheTitreCorrect()
+        public void Accueil()
         {
             _driver.Navigate().GoToUrl("http://localhost:5000");
 
@@ -33,16 +32,16 @@ namespace BookListSeleniumTests
 
 
         [Fact]
-        public void AjouterLivre_FormulaireFonctionne()
+        public void AjouterLivre()
         {
             _driver.Navigate().GoToUrl("http://localhost:5000/Books/Upsert");
 
             System.Threading.Thread.Sleep(5000);
 
 
-            _driver.FindElement(By.Id("Name")).SendKeys("Test1");
-            _driver.FindElement(By.Id("Author")).SendKeys("Auteur test2");
-            _driver.FindElement(By.Id("ISBN")).SendKeys("1234567890°");
+            _driver.FindElement(By.Id("Name")).SendKeys("To Kill a Mockingbird");
+            _driver.FindElement(By.Id("Author")).SendKeys("Harper Leaate");
+            _driver.FindElement(By.Id("ISBN")).SendKeys("9780061120084");
 
             _driver.FindElement(By.CssSelector("form")).Submit();
             System.Threading.Thread.Sleep(5000);
@@ -55,7 +54,7 @@ namespace BookListSeleniumTests
 
 
         [Fact]
-        public void ModifierLivre_Fonctionne()
+        public void ModifierLivre()
         {
             // Aller à la page des livres
             _driver.Navigate().GoToUrl("http://localhost:5000/Books");
@@ -66,7 +65,7 @@ namespace BookListSeleniumTests
             var lignesLivres = _driver.FindElements(By.CssSelector("table tbody tr"));
             foreach (var ligne in lignesLivres)
             {
-                if (ligne.Text.Contains("Test1"))
+                if (ligne.Text.Contains("To Kill a Mockingbird"))
                 {
                     // Cliquer sur le bouton Edit
                     var boutonEdit = ligne.FindElement(By.CssSelector(".btn-success"));
@@ -81,7 +80,7 @@ namespace BookListSeleniumTests
             // Modifier l'auteur
             var champAuteur = _driver.FindElement(By.Id("Author"));
             champAuteur.Clear(); 
-            champAuteur.SendKeys("Auteur modifié avec succes!!!");
+            champAuteur.SendKeys("Harper Lee");
 
             // Soumettre
             _driver.FindElement(By.CssSelector("form")).Submit();
@@ -89,11 +88,11 @@ namespace BookListSeleniumTests
 
             // Vérifier que le nouvel auteur est présent
             var body = _driver.PageSource;
-            Assert.Contains("Auteur modifié avec succes!!!", body);
+            Assert.Contains("Harper Lee", body);
         }
 
         [Fact]
-        public void SupprimerLivre_Fonctionne()
+        public void SupprimerLivre()
         {
             // Aller à la page des livres
             _driver.Navigate().GoToUrl("http://localhost:5000/Books");
@@ -104,7 +103,7 @@ namespace BookListSeleniumTests
             var lignesLivres = _driver.FindElements(By.CssSelector("table tbody tr"));
             foreach (var ligne in lignesLivres)
             {
-                if (ligne.Text.Contains("Livre à modifier"))
+                if (ligne.Text.Contains("L'Étranger"))
                 {
                     var boutonDelete = ligne.FindElement(By.CssSelector(".btn-danger"));
                     boutonDelete.Click();
@@ -112,14 +111,11 @@ namespace BookListSeleniumTests
                 }
             }
 
-            // Attendre que la boîte de dialogue SweetAlert apparaisse
             System.Threading.Thread.Sleep(5000);
 
-            // Cliquer sur le bouton OK dans le pop-up SweetAlert
             var boutonOK = _driver.FindElement(By.CssSelector(".swal-button--confirm.swal-button--danger"));
             boutonOK.Click();
 
-            // Attendre le rafraîchissement
             System.Threading.Thread.Sleep(5000);
 
            
